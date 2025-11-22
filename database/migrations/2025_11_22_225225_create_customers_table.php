fa21dce0f9c9f933da1cba660592c1f92288c25e
Jenya <zhenya.krivec@gmail.com>
2025-11-22 17:24:35 +0200

    Таблицы польсователей и сообщений, которые создаються с помощью миграций

---
+++ database/migrations/2025_11_22_225225_create_customers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('phone', 50)->unique()->index();
+            $table->string('email', 255)->unique()->index();
+            $table->string('token', 255)->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
---
+++ database/migrations/2025_11_22_225320_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id')->index();
+            $table->string('subject', 255)->default('');
+            $table->text('text');
+            $table->boolean('is_incoming')->default(true);
+            $table->enum('status', ['new', 'in progress', 'processed'])->default('new');
+            $table->dateTime('response_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
